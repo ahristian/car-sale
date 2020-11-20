4c5098d7e963a4472d741cd1dfc0ca05340f06c7
ahristian <hristian.andrei@gmail.com>
2020-11-19 23:53:29 -0700

    added cars

--- app/routes/inventory.js
+++ app/routes/inventory.js
@@ -1,23 +1,23 @@
 import Route from '@ember/routing/route';
 
+const COMMUNITY_CATEGORIES = [
+  'cars'
+];
+
 export default class IndexRoute extends Route {
   async model() {
-    return {
-      title: 'Tesla Model 3 for 35.000$',
-      brand: 'Tesla',
-      model: 'Model',
-      mileage: '1k',
-      color: 'White',
-      price: '35.000$',
-      image: 'https://inteng-storage.s3.amazonaws.com/img/iea/XD6K7Knj6v/sizes/teslamodel3_md.jpg',
-      description: 'The Tesla Model 3 is an electric four-door fastback sedan developed by Tesla. The Model 3 Standard' +
-        ' Range Plus version delivers an EPA-rated all-electric range of 263 miles (423 km) and the Long Range versions' +
-        ' deliver 353 miles (568 km). According to Tesla the Model 3 carries full self-driving hardware, with periodic ' +
-        'software updates adding functionality. Limited production of the Model 3 began in mid-2017, with the first ' +
-        'production vehicle rolling off the assembly line on July 7, 2017.',
-    };
-    let response = await fetch('/api/rentals.json');
-    let parsed = await response.json();
-    return parsed;
+    let response = await fetch('/cars-in-stock.json');
+    let { data } = await response.json();
+
+    return data.map(model => {
+      let { attributes } = model;
+      let type;
+
+      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+        type = 'cars';
+      }
+
+      return { type, ...attributes };
+    });
   }
 }
