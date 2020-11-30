e21cd207bf758ebfde9a4bc8189773ffbb9a4cba
ahristian <hristian.andrei@gmail.com>
2020-11-29 20:55:04 -0700

    added validators

--- app/models/vehicle.js
+++ app/models/vehicle.js
@@ -1,7 +1,22 @@
 import DS from 'ember-data';
+import { validator, buildValidations } from 'ember-cp-validations';
 
+const Validations = buildValidations({
+  type: validator('presence', true),
+  brand: validator('presence', true),
+  model: validator('presence', true),
+  year: validator('number', {gt: 1990}),
+  mileage: validator('number', {gt: 0}),
+  color: validator('presence', true),
+  image: validator('presence', true),
+  price: validator('number', {gt: 0}),
+  description: validator('presence', true),
+  name: validator('presence', true),
+  email: validator('presence', true),
+  phone: validator('presence', true),
 
-export default DS.Model.extend ({
+})
+export default DS.Model.extend (Validations, {
   type: DS.attr('string'),
   brand: DS.attr('string'),
   model: DS.attr('string'),
